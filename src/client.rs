d4de6db94dfb83d7af3451084433027fdf012c2b
messense <messense@icloud.com>
2016-02-28 13:33:53 +0800

    Finish sentiment API

--- src/client.rs
+++ src/client.rs
@@ -3,10 +3,10 @@ use std::io::Read;
 use url::Url;
 use hyper::Client;
 use hyper::method::Method;
-use hyper::header::{UserAgent, Accept, ContentLength, qitem};
+use hyper::header::{UserAgent, Accept, ContentLength, ContentType, qitem};
 use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
 use rustc_serialize::{Encodable, Decodable};
-use rustc_serialize::json::{self, Json};
+use rustc_serialize::json::{self, Json, ToJson};
 
 use errors::Error;
 
@@ -72,12 +72,15 @@ impl BosonNLP {
             Err(..) => "".to_owned(),
         };
         let req = self.client.request(method.clone(), url)
-                                 .header(UserAgent(format!("bosonnlp-rs/{}", env!("CARGO_PKG_VERSION"))))
-                                 .header(Accept(vec![qitem(Mime(TopLevel::Application, SubLevel::Json,
-                                                                vec![(Attr::Charset, Value::Utf8)]))]))
-                                 .header(XToken(self.token.clone()));
+                             .header(UserAgent(format!("bosonnlp-rs/{}", env!("CARGO_PKG_VERSION"))))
+                             .header(Accept(vec![qitem(Mime(TopLevel::Application, SubLevel::Json,
+                                                            vec![(Attr::Charset, Value::Utf8)]))]))
+                             .header(XToken(self.token.clone()));
         let mut res = if method == Method::Post {
-            try!(req.body(&body).send())
+            let req = req.header(ContentType(Mime(TopLevel::Application, SubLevel::Json,
+                                             vec![(Attr::Charset, Value::Utf8)])))
+                         .body(&body);
+            try!(req.send())
         } else {
             try!(req.send())
         };
@@ -105,10 +108,78 @@ impl BosonNLP {
         self.request(Method::Get, endpoint, params, &json::Object::new())
     }
 
-    fn post<D, E>(&self, endpoint: &str, params: Vec<(&str, &str)>, data: &E) -> Result<D>
+    fn post<D>(&self, endpoint: &str, params: Vec<(&str, &str)>, data: &Json) -> Result<D>
         where D: Decodable,
-              E: Encodable
     {
         self.request(Method::Post, endpoint, params, data)
+    }
+
+    /// [情感分析接口](http://docs.bosonnlp.com/sentiment.html)
+    ///
+    /// ``contents``: 需要做情感分析的文本序列
+    ///
+    /// ``model``: 使用不同的语料训练的模型
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// extern crate bosonnlp;
+    /// use bosonnlp::BosonNLP;
+    ///
+    /// fn main() {
+    ///     let nlp = BosonNLP::new(env!("BOSON_API_TOKEN"));
+    ///     let rs = nlp.sentiment(&vec!["这家味道还不错".to_owned()], "food").unwrap();
+    ///     assert_eq!(1, rs.len());
+    /// }
+    /// ```
+    pub fn sentiment(&self, contents: &[String], model: &str) -> Result<Vec<(f32, f32)>> {
+        let endpoint = format!("/sentiment/analysis?{}", model);
+        let data = contents.to_vec().to_json();
+        self.post::<Vec<(f32, f32)>>(&endpoint, vec![], &data)
+    }
+
+    /// [新闻分类接口](http://docs.bosonnlp.com/classify.html)
+    pub fn classify(&self, contents: &[String]) -> () {
+        unimplemented!();
+    }
+
+    /// [语义联想接口](http://docs.bosonnlp.com/suggest.html)
+    pub fn suggest(&self, word: &str, top_k: usize) -> () {
+        unimplemented!();
+    }
+
+    /// [关键词提取接口](http://docs.bosonnlp.com/keywords.html)
+    pub fn extract_keywords(&self, text: &str, top_k: usize, segmented: bool) -> () {
+        unimplemented!();
+    }
+
+    /// [依存文法分析接口](http://docs.bosonnlp.com/depparser.html)
+    pub fn depparser(&self, contents: &[String]) -> () {
+        unimplemented!();
+    }
+
+    /// [命名实体识别接口](http://docs.bosonnlp.com/ner.html)
+    pub fn ner(&self, contents: &[String], sensitivity: usize, segmented: bool) -> () {
+        unimplemented!();
+    }
+
+    /// [分词与词性标注接口](http://docs.bosonnlp.com/tag.html)
+    pub fn tag(&self, contents: &[String], space_mode: usize, oov_level: usize, t2s: bool, special_char_conv: bool) -> () {
+        unimplemented!();
+    }
+
+    /// [新闻摘要接口](http://docs.bosonnlp.com/summary.html)
+    pub fn summary(&self, title: &str, content: &str, word_limit: f32, not_exceed: bool) -> () {
+        unimplemented!();
+    }
+
+    /// [文本聚类接口](http://docs.bosonnlp.com/cluster.html)
+    pub fn cluster(&self, contents: &[String], task_id: &str, alpha: f32, beta: f32, timeout: usize) -> () {
+        unimplemented!();
+    }
+
+    /// [典型意见接口](http://docs.bosonnlp.com/comments.html)
+    pub fn comments(&self, contents: &[String], task_id: &str, alpha: f32, beta: f32, timeout: usize) -> () {
+        unimplemented!();
     }
 }
